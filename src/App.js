9cf79f3543bea90d3dd981a876a4d138c00874af
Marco Aguilar <aguilaramarco@yahoo.com>
2019-07-09 22:04:15 -0700

    Cleaned up documentation.

--- src/App.js
+++ src/App.js
@@ -4,36 +4,17 @@ import { articles } from './articles.js'
 import CardComponent from './CardComponent';
 import PageNavComponent from './PageNavComponent';
 
-// ****************************************
-
-// *******************************
-// Later on we need to fix the hardcoded "5" value
-// and store it in a variable somewhere just so the developer
-// can decide how many articles they want displayed on each page.
 class App extends React.Component {
-  // constructor is just used to initialize your values.
-  // Calculate everything in a separate function.
-  // Then call that function in render() before returning
-  // your final result.
-
-  // Remember that calling this.setState()
-  // tells React to call render() again....
-  // SO! any data that needs to make everything render again
-  // should belong to this.state!
+  // Initialize values in constructor.
   constructor(props) {
     super(props);
-    /*
-      numOfArticles is inside our state object because PageNavComponent
-      needs to re-render the number of buttons to display if we get a reponse
-      with a large or small number of values. (For now we're just using a fixed
-      size from our articles.js file AKA our dummy data).
-    */
+
     this.state = {
       numOfArticles: articles.length,
       firstArticleIndex: 0,
       /* 
-        If the number of articles is greater than 5, then
-        get first 5 articles. Else, get what we can.
+        If the number of articles is greater than articlesPerPage, then
+        get first set of articles. Else, get what we can.
       */
       lastArticleIndex: (
         (articles.length > this.props.articlesPerPage) ? 
@@ -44,29 +25,22 @@ class App extends React.Component {
   }
 
   /*
-    This method will be sent PageNavComponent as a callback function.
-    Once the user clicks on a "pagination button" (for a lack of better words),
-    it will execute this callback. This method is responsible for updating the indices
-    of the first and last article that we want to display. React will call render() again
-    with the updated state values, and will ultimately update currentArticles.
+    This callback will be sent PageNavComponent as a prop.
+    Once the user clicks on a button (to navigate to another set of articles),
+    this callback will recieve the index of the last article in the set and update
+    firstArticleIndex and lastArticleIndex accordingly. 
+
+    Calling this.setState() tells React to call render() again, since there's been a change
+    in the component's state. This will ultimately update currentArticles in render();
   */
   updateArticles(lastIndex) {
-    // Called from createButton() in PageNavComponent
     this.setState({
       firstArticleIndex: lastIndex - this.props.articlesPerPage,
       lastArticleIndex: lastIndex
     });
-
   }
 
   render() {
-    /* 
-      Updating first/lastArticleIndex will make React call this function again
-      and send the updated "currentArticles" to our CardComponents.
-      Meanwhile our CardComponent only needs to worry about displaying the data
-      that's passed in through the props object, without needing to manage state.
-    */
-
     // Use slice() NOT splice(). The latter modifies the original array (articles).
     let currentArticles = articles.slice(this.state.firstArticleIndex,
       this.state.lastArticleIndex);
--- src/PageNavComponent.js
+++ src/PageNavComponent.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import './PageNav.css';
 
-// ************************************
-//
-
 class PageNavComponent extends React.Component {
 
+    /*
+        current is used to figure out which button is currently selected.
+        Binding the onClick listeners is required to that the methods know 
+        what "this" (PageNavComponent instance) is referring to.
+    */
     constructor(props) {
         super(props);
-        // current is used to figure out which button is currently selected.
         this.state = {
             current: 0,
             currentButtons: this.getInitialButtons(),
         }
-        // Need this so handleClick knows what this.setState() is referring to
+        
         this.handleClick = this.handleClick.bind(this);
         this.incrementButton = this.incrementButton.bind(this);
         this.decrementButton = this.decrementButton.bind(this);
     }
 
-
+    /*
+        Create buttons based on the numOfArticles and the number of articlesPerPage.
+        If the numbOfArticles has a remainder, add an extra button.
+    */
     getNumOfButtons() {
-        /*
-            Create buttons based on the number of articles.
-            If the number is not perfectly divisible by 5 (hasRemainder),
-            then we add an extra button.
-        */
         let numOfButtons = Math.floor(this.props.numOfArticles / this.props.articlesPerPage);
         const hasRemainder = this.props.numOfArticles % this.props.articlesPerPage !== 0;
 
-        // Need an extra button.
         if(hasRemainder) {
             numOfButtons++;
         }
@@ -37,12 +35,14 @@ class PageNavComponent extends React.Component {
         return numOfButtons;
     }
 
-
+    /*
+        Gets the initial set of buttons. If numOfButtons > 5 then we grab the first
+        5 buttons, because we want to display 5 at a time if possible.
+    */
     getInitialButtons() {
         let buttonNums = [];
         const numOfButtons = this.getNumOfButtons();
 
-        console.log(numOfButtons);
         if(numOfButtons > 5) {
             buttonNums.push(0,1,2,3,4);
         } else {
@@ -68,73 +68,52 @@ class PageNavComponent extends React.Component {
         );
     }
 
-    /*  
-        This will call the callback from the App component and
-        update the current so React knows which button to highlight next.
-    */
 
-    /**
-     * There's a bug here whenever this.props.articlesPerPage > 7
-     * However,
-     *  this bug only shows up when we click on a button (NOT when we
-     *  click on "next" button)
-     * 
-     *  Found the problem....
-     *      there are less numberOfButtons the more
-     *      articlesPerPage you have.
-     * 
-     *      This results in entering the "else-if" statement
-     */
     handleClick(buttonIndex) {
-        let updatedCurrentButtons = [];
-        const numOfButtons = this.getNumOfButtons();
-        
         /*
-            This is sent back to App component's 
-            updateArticles() bc this let's us figure out what
-            number of articlesPerPage to display. 
-            (Bc we only want to show x # of articles
-            at a time)
+            Update the index of the last article that's displayed.
+            This is sent to App component's updateArticles() to update
+            the next set of articles displayed.
         */
         const lastArticleIndex = (buttonIndex + 1) * this.props.articlesPerPage;
         this.props.onClick(lastArticleIndex);
 
-        console.log("numOfButtons");
-        console.log(numOfButtons);
-        console.log("buttonIndex");
-        console.log(buttonIndex);
-        // We're using 5 here bc we only want to display 5 buttons
-        // before using Next and Prev buttons.
+        let updatedCurrentButtons = [];
+        const numOfButtons = this.getNumOfButtons();
+        /* 
+            We're using 5 here bc we only want to display a maximum of 5 buttons
+            before using Next and Prev buttons.
+        */ 
         if(buttonIndex < numOfButtons - 5) {
-            
-            // If user clicks 1 (index 0), then 
-            // updatedCurrentButton = [0, 1, 2, 3, 4] AKA (1, 2, 3, 4, 5)
+            /*
+                The selected button will be the first of 5 buttons displayed.
+                If numOfbuttons < 5, then the else-if statement will be executed.
+            */
             for(let i = 0; i < 5; i++) {
                 updatedCurrentButtons.push(buttonIndex + i);
             }
         } else if (buttonIndex >= numOfButtons - 5) {
-            console.log("In!");
-            // Display the last 5 buttons.
-            const fifthToLast = numOfButtons - 5;
+            /*
+                If buttonIndex is within the last 5 possible buttons,
+                then updatedCurrentButtons will display the last 5 buttons.
 
-            // if fifthToLast < 0 
-            // then don't update currentButtons.
-            /**
-             * We do this because fifth to last could be -1
-             * if numOfButtons < 5, due to articlesPerPage being
-             * a high number (Ex: Greater than 7)
-             */
-            if(fifthToLast < 0) {
+                However, if numOfButtons < 5, then we use the old 
+                currentButtons because we can't grab the last
+                5 buttons (Bc there's not enough buttons to grab).
+                numOfButtons decreases as the articlesPerPages increases.
+                
+            */
+            if(numOfButtons < 5) {
+                // Use old currentButtons.
                 updatedCurrentButtons = this.state.currentButtons;
             } else {
+                // Display the last 5 buttons.
+                const fifthToLast = numOfButtons - 5;
                 for(let i = 0; i < 5; i++) {
                     updatedCurrentButtons.push(fifthToLast + i);
                 }
-            }
-            
-            
+            }   
         }
-        
         
         this.setState({
             current: buttonIndex,
@@ -143,18 +122,21 @@ class PageNavComponent extends React.Component {
     }
 
 
+    /*
+        Increment/Update "currentButtons" by 1. However, if we're already displaying the 
+        last 5 buttons. Then only update "current".
+    */
     incrementButton() {
         const updatedCurrentButtons = this.state.currentButtons.map(x => x + 1);
         const updatedCurrent = this.state.current + 1;
 
-        // Using updatedCurrent, we need to make sure to call the callback with
-        // the new list of articles we want to display.
+        /* 
+            Using updatedCurrent, call the callback in our props object.
+            This will execute App component's updateArticle()
+        */
         const lastArticleIndex = (updatedCurrent + 1) * this.props.articlesPerPage;
         this.props.onClick(lastArticleIndex);
-        /*
-         *  If the user clicks the "next" button, but we're already
-         *  displaying the last 5 buttonss possible, then only update current.
-         */
+
         if(this.state.current < this.getNumOfButtons() - 5) {
             this.setState({
                 current: updatedCurrent,
@@ -164,57 +146,47 @@ class PageNavComponent extends React.Component {
             this.setState({
                 current: updatedCurrent,
             });
-        }
-            
-        
+        }    
     }
 
-    // ********* The Bug is here
-    // Once we reach 6,7,8, or even 9....we don't see
-    // the next buttons down (5,4,3,2,1)... 
-    // And I'm pretty sure the problem is here somewhere
+
+    /*  
+        Similar to incrementButton(), except we decrement currentButtons by 1.
+        However, if the first element in updatedCurrentButtons is -1, then we only
+        update "current".
+    */
     decrementButton() {
         const updatedCurrentButtons = this.state.currentButtons.map(x => x - 1);
         const updatedCurrent = this.state.current - 1;
 
         const lastArticleIndex = (updatedCurrent + 1) * this.props.articlesPerPage;
         this.props.onClick(lastArticleIndex);
-        
-        // ALMOST FIXED IT!!!
-        // If the first item in updatedCurrentButtons === -1
-        // Then we just update current because we don't want to
-        // have a button display "0" or "-1"
+
         if(updatedCurrentButtons[0] === -1) {
-            // currentButtons may contain negative values if they keep
-            // clicking "previous" button.
             this.setState({
                 current: updatedCurrent,
-            });
-            
+            }); 
         } else {
             this.setState({
                 current: updatedCurrent,
                 currentButtons: updatedCurrentButtons,
             });
         }
-
     }
 
 
     render() {
         let navButtons = [];
         /*
-            If the currently selected button is not the first, then display
-            the "prev" button.
-            We're using -2 as a key bc no other button will be using it as its key.
+            Display the "Prev" button if we're not on the first button.
+            We're using -1 as a key bc no other button will be using it as its key.
         */
         if(this.state.current !== 0) {
             navButtons.push(
-                <button key={-2} onClick={this.decrementButton}>Prev</button>
+                <button key={-1} onClick={this.decrementButton}>Prev</button>
             );
         }
            
-
         // Dynamically create buttons based on currentButtons 
         this.state.currentButtons.forEach(index => {
             if(index === this.state.current) {
@@ -226,13 +198,12 @@ class PageNavComponent extends React.Component {
         });
 
         /*
-            If the currently selected button is not the last, then display
-            the "next" button.
+            Display the "Next" button if we're not on the last button.
             We're using -1 as a key bc no other button will be using it as its key.
         */
         if(this.state.current !== (this.getNumOfButtons() - 1)) {
             navButtons.push(
-                <button key={-1} onClick={this.incrementButton}>Next</button>
+                <button key={-2} onClick={this.incrementButton}>Next</button>
             );
         }
 
